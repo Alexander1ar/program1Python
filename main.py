0995398c33f2467e3b5e1bc17881523b9d43a6cf
Tu nombre <Tu correo>
2024-09-05 23:07:44 -0500

    cambios 8

--- main.py
+++ main.py
@@ -170,6 +170,6 @@ def correlation():
         return "No variables selected. Go back and select two variables."
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     from waitress import serve
     serve(app, host="0.0.0.0", port=port)
